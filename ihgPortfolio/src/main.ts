7f1333e49b3f8040a300eeda1b68c7bdfb361e53
Irene <ihuebrag@purdue.edu>
2024-07-02 21:29:11 -0400

    changed folder loc and name

--- ihgPortfolio/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrapApplication, provideProtractorTestingSupport } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
-import routeConfig from './app/routes';
-
-bootstrapApplication(AppComponent, {
-  providers: [provideProtractorTestingSupport(), provideRouter(routeConfig)],
-  // activates routing ^^
-}).catch((err) => console.error(err));
